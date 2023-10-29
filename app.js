8ea743c17af9631640af325280cd1c9bdd68cd4d
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2023-10-29 18:47:30 +0530

    Implement Dynamic Grid Resizing
    Description:
    
    Added functionality to dynamically resize the grid based on user input.
    Created the createGrid function to generate grids with the specified size.
    Implemented grid column and row templates for equal spacing.
    Modified the promptForGridSize function to handle user input and enforce a limit of 100 squares per side.
    Added an alert for input values exceeding the limit.
    Configured the "resize" button to trigger the grid resizing prompt.

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const gridContainer = document.querySelector(".grid-container");
+const resizeButton = document.getElementById("resize-button");
 
-for (let i = 0; i < 256; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-    gridContainer.appendChild(gridItem);
+function createGrid(size) {
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        gridContainer.appendChild(gridItem);
+    }
 }
+
+
+
+function promptForGridSize() {
+    const newSize = prompt("Enter the number of squares per side of the new grid");
+    if (newSize <= 100) {
+        createGrid(newSize);
+    }
+    else {
+        alert('Please input values less than or equal to 100');
+    }
+}
+
+resizeButton.addEventListener("click", promptForGridSize);
+
+createGrid(16);
